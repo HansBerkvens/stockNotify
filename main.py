28a7591dbc2deb9c9838b0b332e549f8246a94bf
HansBerkvens <hans_berkvens@hotmail.com>
2026-09-21 23:32:01 +0200

    final

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import logging
 import sys
 logging.basicConfig(level=logging.INFO, format="%(name)s:%(levelname)s:%(asctime)s\t\t%(message)s", datefmt="%H:%M:%S", force=True, stream=sys.stdout)
 
-print('At least the logger works')
+print('At least the logger works', flush=True)
 
 
 send_notification('Stock Notify', 'Starting run')
@@ -14,19 +14,20 @@ sentences = []
 for s in STOCKS:
     try:
         price = yf.Ticker(s.ticker).history(period=f'1d', auto_adjust=False)['Close'].item()
-        print(f'{s.ticker} = {price:.2f}')
+        print(f'{s.ticker} = {price:.2f}', flush=True)
         if s.price_min is not None and price <= s.price_min:
             sentences.append(f'Ticker {s.ticker} ({s.alias}) fell below {s.price_min}.')
         elif s.price_max is not None and s.price_max <= price:
             sentences.append(f'Ticker {s.ticker} ({s.alias}) rose above {s.price_min}.')
         else:
             sentences.append(f'Ticker {s.ticker} is at {price:.2f} which is between {s.price_min} and {s.price_max}')
-    except Exception:
+    except Exception as e:
+        print(str(e).split('\n'), flush=True)
         pass
 
 if sentences:
-    print(f'Trying to send {len(sentences)} sentences.')
+    print(f'Trying to send {len(sentences)} sentences.', flush=True)
     body = '\n'.join(sentences)
-    print(body)
+    print(body, flush=True)
     # send_notification('Stock Notify Action', body)
 
